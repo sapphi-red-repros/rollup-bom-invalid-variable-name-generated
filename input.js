248a3af75d59bde26e2d757e2326997af180f3fe
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-09-30 16:24:01 +0900

    Init

---
+++ input.js
@@ -0,0 +1,4 @@
+// import from a utf-8 bom file
+import { msg as bomMsg } from './foo.js'
+const msg = 'foo'
+console.log(msg, bomMsg)
---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: './input.js',
+  output: {
+    dir: 'dist',
+    format: 'es'
+  }
+}
